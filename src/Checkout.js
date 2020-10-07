cd4d24852d2b8e8d09236ed63a50762276adb94b
itsmeboris <boris.sobol@gmail.com>
2020-10-07 13:13:53 +0300

    Update Checkout.js

--- src/Checkout.js
+++ src/Checkout.js
@@ -15,7 +15,6 @@ function Checkout() {
           alt=""
         />
         <div>
-          // hello
           <h3>Hello, {user?.email}</h3>
           <h2 className="checkout__title">Your shopping Basket</h2>
           {basket.map((item) => (
